67f517e326c6fc15cc53b890176766d8a09a6555
Abid Miah <abid.miah@nfocus.co.uk>
2024-04-16 17:31:48 +0100

    Change for to foreach DDT

--- tests/eCommerce.spec.ts
+++ tests/eCommerce.spec.ts
@@ -6,34 +6,34 @@ import couponData from "./JSONData/CouponCodes.json"
 
 test.describe("eCommerce Tests", () => {
 
-  for (const { code, discount } of couponData) {
-    test(`Applying Coupon - '${code}'`, async ({ page, addItemLogoutFix, cartPOM }, testInfo) => {
-      // Validates the entered coupon
-      await (await cartPOM.EnterCoupon(code)).ApplyCoupon();
-      await expect(cartPOM.alertMessage(), `Coupon Code '${code}, does not exist!`).not.toContainText("does not exist!");
-      console.log(`Valid Coupon Applied: '${code}'`);
+  couponData.forEach(coupon => {
+      test(`Applying Coupon - '${coupon.code}' `, async ({ page, addItemLogoutFix, cartPOM }, testInfo) => {
+        // Validates the entered coupon
+        await (await cartPOM.EnterCoupon(coupon.code)).ApplyCoupon();
+        await expect(cartPOM.alertMessage(), `Coupon Code '${coupon.code}, does not exist!`).not.toContainText("does not exist!");
+        console.log(`Valid Coupon Applied: '${coupon.code}'`);
 
-      const expectedDiscount = discount; // Expected Discount of 25% for nfocus. 15% for edgewords
-      const actualDiscount = await cartPOM.GetDiscountPercentage(code); // Calculates actual discount
-      await expect(actualDiscount, `Expected ${expectedDiscount}% off, Actual ${actualDiscount}% off instead`).toBe(expectedDiscount);
+        const expectedDiscount = coupon.discount; // Expected Discount of 25% for nfocus. 15% for edgewords
+        const actualDiscount = await cartPOM.GetDiscountPercentage(coupon.code); // Calculates actual discount
+        await expect(actualDiscount, `Expected ${expectedDiscount}% off, Actual ${actualDiscount}% off instead`).toBe(expectedDiscount);
 
-      const calculatedTotal = await cartPOM.CalculateTotal(code);
-      const grandTotalPrice = await cartPOM.GetGrandTotalPrice();
+        const calculatedTotal = await cartPOM.CalculateTotal(coupon.code);
+        const grandTotalPrice = await cartPOM.GetGrandTotalPrice();
 
-      // Validates that the discount has been applied correctly with the total price
-      await expect(grandTotalPrice, "Discount not applied correctly!").toBe(calculatedTotal);
-      console.log(`Expected total value: £${grandTotalPrice}, Actual total value: £${calculatedTotal}`);
+        // Validates that the discount has been applied correctly with the total price
+        await expect(grandTotalPrice, "Discount not applied correctly!").toBe(calculatedTotal);
+        console.log(`Expected total value: £${grandTotalPrice}, Actual total value: £${calculatedTotal}`);
 
-      // Wait for js scripts on page to finish before screenshotting element
-      await page.waitForLoadState("networkidle");
-      let date = new Date().toLocaleString();
-      date = date.split("/").join("-").split(":").join("-");
+        // Wait for js scripts on page to finish before screenshotting element
+        await page.waitForLoadState("networkidle");
+        let date = new Date().toLocaleString();
+        date = date.split("/").join("-").split(":").join("-");
 
-      await cartPOM.cartTotalElement().screenshot({ path: `./Screenshots/Cart Total, ${date}.png` })
-      console.log("Attaching 'Cart Total' screenshot to report")
-      await testInfo.attach('Cart Total', { path: `./Screenshots/Cart Total, ${date}.png` })
+        await cartPOM.cartTotalElement().screenshot({ path: `./Screenshots/Cart Total, ${date}.png` })
+        console.log("Attaching 'Cart Total' screenshot to report")
+        await testInfo.attach('Cart Total', { path: `./Screenshots/Cart Total, ${date}.png` })
     });
-  }
+  });
 
   test("Checkout Process", async ({ page, addItemLogoutFix, cartPOM, checkoutPOM, orderInfoPOM, navPOM, accountPOM, allOrdersPOM }, testInfo) => {
     // Navigate to Checkout
