417dd9a9dd2218d22560e87e9e9777c12ec8531f
Abid Miah <abid.miah@nfocus.co.uk>
2024-04-17 10:44:00 +0100

    Change seperate test files

--- tests/checkoutProcess.spec.ts
+++ tests/checkoutProcess.spec.ts
@@ -1,41 +1,8 @@
 import { test, expect } from "./Utils/AddItemLogoutFixture";
 import Customer from "./Utils/Customer";
 import billingDetails from "./JSONData/BillingDetails.json"
-import couponData from "./JSONData/CouponCodes.json"
 
-
-test.describe("eCommerce Tests", () => {
-
-  couponData.forEach(coupon => {
-      test(`Applying Coupon - '${coupon.code}' `, async ({ page, addItemLogoutFix, cartPOM }, testInfo) => {
-        // Validates the entered coupon
-        await (await cartPOM.EnterCoupon(coupon.code)).ApplyCoupon();
-        await expect(cartPOM.alertMessage(), `Coupon Code '${coupon.code}, does not exist!`).not.toContainText("does not exist!");
-        console.log(`Valid Coupon Applied: '${coupon.code}'`);
-
-        const expectedDiscount = coupon.discount; // Expected Discount of 25% for nfocus. 15% for edgewords
-        const actualDiscount = await cartPOM.GetDiscountPercentage(coupon.code); // Calculates actual discount
-        await expect(actualDiscount, `Expected ${expectedDiscount}% off, Actual ${actualDiscount}% off instead`).toBe(expectedDiscount);
-
-        const calculatedTotal = await cartPOM.CalculateTotal(coupon.code);
-        const grandTotalPrice = await cartPOM.GetGrandTotalPrice();
-
-        // Validates that the discount has been applied correctly with the total price
-        await expect(grandTotalPrice, "Discount not applied correctly!").toBe(calculatedTotal);
-        console.log(`Expected total value: £${grandTotalPrice}, Actual total value: £${calculatedTotal}`);
-
-        // Wait for js scripts on page to finish before screenshotting element
-        await page.waitForLoadState("networkidle");
-        let date = new Date().toLocaleString();
-        date = date.split("/").join("-").split(":").join("-");
-
-        await cartPOM.cartTotalElement().screenshot({ path: `./Screenshots/Cart Total, ${date}.png` })
-        console.log("Attaching 'Cart Total' screenshot to report")
-        await testInfo.attach('Cart Total', { path: `./Screenshots/Cart Total, ${date}.png` })
-    });
-  });
-
-  test("Checkout Process", async ({ page, addItemLogoutFix, cartPOM, checkoutPOM, orderInfoPOM, navPOM, accountPOM, allOrdersPOM }, testInfo) => {
+test("Checkout Process", async ({ page, addItemLogoutFix, cartPOM, checkoutPOM, orderInfoPOM, navPOM, accountPOM, allOrdersPOM }, testInfo) => {
     // Navigate to Checkout
     await cartPOM.GoToCheckout();
 
@@ -96,4 +63,3 @@ test.describe("eCommerce Tests", () => {
     console.log("Attaching 'All Orders' screenshot to report");
     await testInfo.attach('All Orders', { path: `./Screenshots/All Orders, ${date}.png` })
   })
-});
