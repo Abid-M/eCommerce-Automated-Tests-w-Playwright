f5a2c645e3a9dc64da0d74683f18e3d03e2456c9
Abid Miah <abid.miah@nfocus.co.uk>
2024-04-23 13:33:16 +0100

    Update data files

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,7 @@
+/*
+This code declares a new module `NodeJS` and interface `ProcessEnv` in the global namespace. 
+The `ProcessEnv` interface extends the `NodeJS.ProcessEnv` interface and defines the types for my environment variables.
+*/
 export {}
 declare global {
     namespace NodeJS {
@@ -7,4 +11,3 @@ declare global {
         }
     }
 }
-
