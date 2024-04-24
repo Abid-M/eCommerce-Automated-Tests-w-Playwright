023a1ce39af50d94a55e76ea23e04fde34b9dc7e
abid-m <abid1702@outlook.com>
2024-04-24 12:23:10 +0100

    .

--- tests/POMPages/NavPOM.ts
+++ tests/POMPages/NavPOM.ts
@@ -4,14 +4,14 @@ import { BasePOM, ShopPOM } from "./index.ts"
 class NavPOM extends BasePOM {
     // Locators
     private shopLink = () => this.page.locator('#menu-item-43').getByRole('link', { name: 'Shop' });
-    private cartLink = () => this.page.locator('#menu-item-44').getByRole('link', { name: 'Cart' })
-    private accountLink = () => this.page.locator('#menu-main').getByRole('link', { name: 'My account' })
+    private cartLink = () => this.page.locator('#menu-item-44').getByRole('link', { name: 'Cart' });
+    private accountLink = () => this.page.locator('#menu-main').getByRole('link', { name: 'My account' });
     private dismissBannerLink = () => this.page.getByRole("link", { name: "Dismiss" });
 
     /* Navigates to the shop page. */
     async goToShop(): Promise<ShopPOM> {
         await this.shopLink().click();
-        console.log("Navigated to the Shop Page")
+        console.log("Navigated to the Shop Page");
 
         return new ShopPOM(this.page);
     }
@@ -19,13 +19,13 @@ class NavPOM extends BasePOM {
     /* Navigates to the cart page. */
     async goToCart() {
         await this.cartLink().click();
-        console.log("Navigated to the Cart Page")
+        console.log("Navigated to the Cart Page");
     }
 
     /* Navigates to the cart page. */
     async goToAccount() {
         await this.accountLink().click();
-        console.log("Navigated to the Account Page")
+        console.log("Navigated to the Account Page");
 
         return new MyAccountPOM(this.page);
     }
